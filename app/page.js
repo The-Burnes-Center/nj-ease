334f9d57ad65173a6d721782641d048588f77b46
Kaushik Manivannan <manivannan.k@northeastern.edu>
2025-04-30 14:28:04 -0400

    Fix: UI Changes

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import DocumentValidator from '@/components/DocumentValidator';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-8">
+    <main className="flex min-h-screen flex-col items-center justify-center p-8 bg-black">
       <DocumentValidator />
     </main>
   );
